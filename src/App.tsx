c333ea38ad8e8340d2f860553c721b41cc70772c
SanyaSkl <90842468+SanyaSkl@users.noreply.github.com>
2024-04-17 17:07:07 +0300

    todolist lesson two: filter method  refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import {Todolist} from './components/Todolist';
+import {TaskType, Todolist} from './components/Todolist';
+
+export type FilterValuesType = "all" | "completed" | "active"
 
 function App() {
 
-  let tasks = [
+  let [tasks, setTasks] = useState<Array<TaskType>>([
     { id: 1, title: "CSS&HTML", isDone: true },
     { id: 2, title: "JS", isDone: true },
     { id: 3, title: "React", isDone: false },
     { id: 4, title: "Redux", isDone: false }
-  ]
+  ]);
+    let [filter, setFilter] = useState<FilterValuesType>("all");
 
     function removeTask(id: number) {
-      let resultTask = tasks.filter( t => t.id !== id)
+      let filteredTasks = tasks.filter( t => t.id !== id);
+    }
+
+    function changeFilter(value: FilterValuesType) {
+        setFilter(value);
+    }
+
+    let tasksForTodolist = tasks;
+    if (filter === "completed") {
+        tasksForTodolist = tasks.filter(t => t.isDone === true);
+    }
+    if (filter === "active") {
+        tasksForTodolist = tasks.filter(t => t.isDone === false);
     }
 
   return (
       <div className="App">
         <Todolist title="What to learn"
-                  tasks = {tasks}
+                  tasks = {tasksForTodolist}
                   removeTask = {removeTask}
-
+                  changeFilter = {changeFilter}
         />
       </div>
   );
