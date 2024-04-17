73a88b3af365443bd804611d51ab0c4af5676b10
SanyaSkl <90842468+SanyaSkl@users.noreply.github.com>
2024-04-17 12:35:31 +0300

    todolist lesson two: added map method

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,8 @@ function App() {
   let tasks1 = [
     { id: 1, title: "CSS&HTML", isDone: true },
     { id: 2, title: "JS", isDone: true },
-    { id: 3, title: "React", isDone: false }
+    { id: 3, title: "React", isDone: false },
+    { id: 4, title: "Redux", isDone: false }
   ]
 
   let tasks2 = [
