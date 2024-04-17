e2f6664183cc49e49725ff7dceaa1815d523c76d
SanyaSkl <90842468+SanyaSkl@users.noreply.github.com>
2024-04-17 10:59:42 +0300

    lesson one todolist

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import './App.css';
+import {Todolist} from './components/Todolist';
 
 function App() {
+
+  let tasks1 = [
+    { id: 1, title: "CSS&HTML", isDone: true },
+    { id: 2, title: "JS", isDone: true },
+    { id: 3, title: "React", isDone: false }
+  ]
+
+  let tasks2 = [
+    { id: 1, title: "Legend", isDone: true },
+    { id: 2, title: "The Take", isDone: true },
+    { id: 3, title: "Breaking Bad", isDone: true }
+  ]
+
   return (
-    <div className="App">
-todolist
-    </div>
+      <div className="App">
+        <Todolist title="What to learn" tasks = {tasks1}/>
+        <Todolist title="Movies" tasks = {tasks2}/>
+      </div>
   );
 }
+
 
 export default App;
