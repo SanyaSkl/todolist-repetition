d726aa95fcdad76151afb87fd90715c3d207f7af
SanyaSkl <90842468+SanyaSkl@users.noreply.github.com>
2026-02-02 13:08:52 +0300

    08.1 - Todolist's logic has been rewritten using TDD.

--- src/App.tsx
+++ src/App.tsx
@@ -105,6 +105,7 @@ export const App = () => {
     const changeTaskTitle = (todolistId: string, taskId: string, title: string) => {
         setTasks({...tasks, [todolistId]: tasks[todolistId].map(task => task.id === taskId ? {...task, title} : task)})
     }
+
     const changeTodolistTitle = (todolistId: string, title: string) => {
         setTodolists(todolists.map(todolist => todolist.id === todolistId ? {...todolist, title} : todolist))
     }
--- src/model/todolist-reducer.test.ts
+++ src/model/todolist-reducer.test.ts
@@ -1,21 +1,29 @@
 import {v1} from 'uuid';
-import {expect, test} from 'vitest'
+import {beforeEach, expect, test} from 'vitest'
 import type {Todolist} from '../App.tsx';
-import {deleteTodolistAC, todolistsReducer} from './todolists-reducer.ts';
+import {
+    changeTodolistFilterAC,
+    changeTodolistTitleAC,
+    createTodolistAC,
+    deleteTodolistAC,
+    todolistsReducer
+} from './todolists-reducer.ts';
 
+let todolistId1: string
+let todolistId2: string
+let startState: Todolist[] = []
 
-test('correct todolist should be deleted', () => {
-    // Создание общего ID для каждого todolist-a
-    const todolistId1 = v1()
-    const todolistId2 = v1()
+beforeEach(() => {
+    todolistId1 = v1()
+    todolistId2 = v1()
 
-    // 1. Стартовый state
-    const startState: Todolist[] = [
+    startState = [
         {id: todolistId1, title: 'What to learn', filter: 'all'},
         {id: todolistId2, title: 'What to buy', filter: 'all'}
     ]
+})
 
-    // 2. Действие
+test('correct todolist should be deleted', () => {
     const endState = todolistsReducer(startState, deleteTodolistAC(todolistId1))
 
     // 3. Проверка, что действие изменило state соответствующим образомэ
@@ -23,4 +31,28 @@ test('correct todolist should be deleted', () => {
     expect(endState.length).toBe(1)
     // удалится нужный todolist, не любой
     expect(endState[0].id).toBe(todolistId2)
+})
+
+test('correct todolist should be created', () => {
+    const title = 'New todolist'
+    const endState = todolistsReducer(startState, createTodolistAC(title))
+
+    expect(endState.length).toBe(3)
+    expect(endState[2].title).toBe(title)
+})
+
+test('correct todolist should change its tittle', () => {
+    const title = 'New Title'
+    const endState = todolistsReducer(startState, changeTodolistTitleAC({id: todolistId2, title}))
+
+    expect(endState[0].title).toBe('What to learn')
+    expect(endState[1].title).toBe(title)
+})
+
+test('correct todolist should change its filter', () => {
+    const filter = 'completed'
+    const endState = todolistsReducer(startState, changeTodolistFilterAC({id: todolistId2, filter}))
+
+    expect(endState[0].filter).toBe('all')
+    expect(endState[1].filter).toBe(filter)
 })
