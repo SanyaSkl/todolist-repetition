1210c89b2a3f96b3b7ba4a6cb6a6740965813f6f
SanyaSkl <90842468+SanyaSkl@users.noreply.github.com>
2026-05-07 20:31:59 +0300

    20 - Advanced cache tags

--- src/features/todolist/api/tasksApi.ts
+++ src/features/todolist/api/tasksApi.ts
@@ -7,25 +7,25 @@ export const tasksApi = baseApi.injectEndpoints({
   endpoints: (build) => ({
     getTasks: build.query<GetTasksResponse, string>({
       query: (todolistId) => `todo-lists/${todolistId}/tasks`,
-      providesTags: ["Task"],
+      providesTags: (_result, _error, todolistId) => [{ type: "Task", id: todolistId }],
     }),
     createTask: build.mutation<TaskOperationResponse, { todolistId: string; title: string }>({
       query: ({ todolistId, title }) => {
         return { method: "post", url: `/todo-lists/${todolistId}/tasks`, body: { title } }
       },
-      invalidatesTags: ["Task"],
+      invalidatesTags: (_result, _error, { todolistId }) => [{ type: "Task", id: todolistId }],
     }),
     deleteTask: build.mutation<DefaultResponse, { todolistId: string; taskId: string }>({
       query: ({ todolistId, taskId }) => {
         return { method: "delete", url: `/todo-lists/${todolistId}/tasks/${taskId}` }
       },
-      invalidatesTags: ["Task"],
+      invalidatesTags: (_result, _error, { todolistId }) => [{ type: "Task", id: todolistId }],
     }),
     updateTask: build.mutation<TaskOperationResponse, { todolistId: string; taskId: string; model: UpdateTaskModel }>({
       query: ({ todolistId, taskId, model }) => {
         return { method: "put", url: `/todo-lists/${todolistId}/tasks/${taskId}`, body: model }
       },
-      invalidatesTags: ["Task"],
+      invalidatesTags: (_result, _error, { todolistId }) => [{ type: "Task", id: todolistId }],
     }),
   }),
 })
